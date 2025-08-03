97786343b49a77ef93fa982f0196f0dcd6ce2d03
Denis Mozhayskiy <den4ikk007@gmail.com>
2025-08-03 16:28:58 +0300

    settings material fix

---
+++ Assets/_Scripts/Cosmetic/StaticSFX.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class StaticSFX : MonoBehaviour
+{
+    [SerializeField] private SoundData sound;
+
+    private AudioSource _audioSource;
+    private AudioManager _audioManager;
+    private void Start()
+    {
+        _audioManager = ServiceLocator.Get<AudioManager>();
+        _audioSource = _audioManager.Play(sound, transform.position);
+    }
+
+    private void OnDestroy()
+    {
+        _audioManager.StopSound(_audioSource);
+    }
+}
